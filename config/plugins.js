5bf81833015fbb80bcf259a0d0357de217d61b4f
Yuliia <julia.av.ferreira@gmail.com>
2021-05-30 16:16:17 +0200

    oppdated images in cloudinary

--- config/plugins.js
+++ config/plugins.js
@@ -7,4 +7,15 @@ module.exports = ({ env }) => ({
       api_secret: env("CLOUDINARY_SECRET"),
     },
   },
+  email: {
+    provider: 'sendgrid',
+    providerOptions: {
+      apiKey: env('SENDGRID_API_KEY'),
+    },
+    settings: {
+      defaultFrom: 'no-reply@strapi.io',
+      defaultReplyTo: 'ulavdeeva960@gmail.com',
+      testAddress: 'ulavdeeva960@gmail.com',
+    },
+  },
 });
